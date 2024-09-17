18e12597e46f73ec14ded2a145c4f293bf75d99e
Juan Sebastian Odoñez Sierra <juanseordo2206@outlook.com>
2024-09-17 08:06:28 -0500

    Agregar cambios para la cadena de conexion

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 // Conexión a la base de datos mongodb
-mongoose.connect('mongodb://localhost:27017/userscoursesdb', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://juansordonez:<db_password>@cluster0.vq7l1.mongodb.net/', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Conectado a MongoDB...'))
     .catch(err => console.log('No se pudo conectar con MongoDB...', err));
 
@@ -20,5 +20,5 @@ app.use('/api/cursos', cursos);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Api REST Ok, y ejecutándose...`);
+    console.log(`Api REST Ok, y ejecutándose...`);5VVx5+3k
 });
